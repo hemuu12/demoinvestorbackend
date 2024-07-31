443dbee799e213cab052b156943feb72e2975d13
hemuu12 <harisingh.bisht2001@gmail.com>
2024-07-31 11:14:25 +0530

    [Review_page]:created review page

--- index.js
+++ index.js
@@ -15,8 +15,18 @@ connectDB();
 // Middleware
 app.use(bodyParser.json());
 
-app.use(cors())
-
+const corsOptions = {
+    origin: 'http://localhost:3000', // Allow requests from this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Allow credentials (e.g., cookies, authorization headers)
+    optionsSuccessStatus: 204 // Some legacy browsers choke on 204
+  };
+  
+  app.use(cors(corsOptions));
+  
+  // Handle Preflight Requests
+  app.options('*', cors(corsOptions));
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/profiles', profileRoutes);
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const app = require('./index');
-const cors = require('cors');
 
 const PORT =  5000;
-app.use(cors())
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
